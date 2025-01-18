f3ca7ad827e07215d4af0702ba3ec5148b64905b
Wisdom <94754503+wizzidom@users.noreply.github.com>
2025-01-18 15:12:57 +0200

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,19 @@ import { useEffect, useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 import { faCircleInfo } from "@fortawesome/free-solid-svg-icons";
+
 export default function App() {
   const apiKey = import.meta.env.VITE_API_KEY;
   const api = "https://api.nasa.gov/planetary/apod?";
 
   const [data, setData] = useState([]);
+  const [display, setDisplay] = useState(false);
+  function toggleDisplay() {
+    display === false ? setDisplay(true) : setDisplay(false);
+  }
+  function bodyTouched() {
+    display === true ? setDisplay(false) : display;
+  }
   useEffect(() => {
     async function fetchData() {
       let day = Math.floor(Math.random() * (28 - 1 + 1)) + 1;
@@ -24,17 +32,29 @@ export default function App() {
   }, []);
 
   return (
-    <div className="nasa-body" style={{ backgroundImage: `url(${data.url})` }}>
+    <div
+      className="nasa-body"
+      style={{ backgroundImage: `url(${data.url})` }}
+      onClick={bodyTouched}
+    >
       <div className="des">
         <h3>Apod Project</h3>
         <br />
         <div className="cons">
           <h1>{data.title}</h1>
-          <div className="info">
+          <div className="info" onClick={toggleDisplay}>
             <FontAwesomeIcon icon={faCircleInfo} />
           </div>
         </div>
       </div>
+
+      {display && (
+        <div className="description">
+          <p>{data.explanation}</p>
+          <br />
+          <p>Date: {data.date}</p>
+        </div>
+      )}
     </div>
   );
 }
